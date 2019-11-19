35aae4d9a5855f24ac985e89ebdf8c8d5a63ecf2
4cruzeta <4cruzetam@gmail.com>
2019-11-19 14:57:44 -0300

    Usando GitIgnore do V. Studio

---
+++ GitIgnoreVs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitIgnoreVs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Teste para verificar uso do Github.\n" +
+							 "A dúvida é se mantenho o arquivo\n" +
+							 ".gitignore original criado pelo\n" +
+							 "VS 2019, ou uso o divulgado no\n" +
+							 "Próprio Github. Nesse caso foi\n" +
+                             "usado o original do V. Studio 2019.");
+		}
+    }
+}
